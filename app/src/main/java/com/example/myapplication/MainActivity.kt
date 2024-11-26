1b6af1a9e2142f436e12a7687c2f09a14fbaa8e6
Muhammad Hamza Azhar <hamzaazharcs@gmail.com>
2024-11-26 16:43:11 -0500

    Scaffolding of the entire view.

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +26,53 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            MyApplicationTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+            MyApp {
+                MainContent()
             }
         }
     }
 }
 
+//This is an example of a container function.
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(content: @Composable () -> Unit) {
+    MyApplicationTheme {
+       Scaffold(
+           topBar = {
+               TopAppBar(
+                   colors = topAppBarColors(
+                       containerColor = MaterialTheme.colorScheme.primaryContainer,
+                       titleContentColor = MaterialTheme.colorScheme.primary,
+                   ),
+                   title = {
+                       Text("Movies")
+                   }
+               )
+           }
+       ) { paddingValues -> // Add the paddingValues parameter
+           Column(
+               modifier = Modifier.padding(paddingValues) // Apply padding here
+           ) {
+               content()
+           }
+       }
+    }
+}
+
+@Composable
+fun MainContent() {
+    Surface {
+        Text("Hello")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     MyApplicationTheme {
-        Greeting("Android")
+        MyApp {
+            MainContent()
+        }
     }
 }
